a324b33a7e5d9c33464fc5e093b1e0ec4b521965
Munteanu Victor <munteanu.victor0201@gmail.com>
2023-02-12 01:28:31 +0200

    add .clang-format

--- tests/NamespaceRenameTest.cpp
+++ tests/NamespaceRenameTest.cpp
@@ -3,46 +3,53 @@
 #ifdef OLD_CODE_ENABLED
 // rename v1 to v2
 namespace path::to::v1 {
-    struct Bar {};
-    constexpr int VAL = 42;
-    enum SomeEnum { A, B, C };
-}
+	struct Bar {};
+	constexpr int VAL = 42;
+	enum SomeEnum { A, B, C };
+	void f(const Bar& val = Bar()) {
+		(void)val;
+	}
+}  // namespace path::to::v1
 #else
 // simply rename
 namespace path::to::v2 {
-  struct Bar {};
-  constexpr int VAL = 42;
-  enum SomeEnum { A, B, C };
-  void f(const Bar& val = Bar()) { (void)val; }
-}
+	struct Bar {};
+	constexpr int VAL = 42;
+	enum SomeEnum { A, B, C };
+	void f(const Bar& val = Bar()) {
+		(void)val;
+	}
+}  // namespace path::to::v2
 
 // add a using namespace
 //  in the old namespace
 namespace path::to::v1 {
-  using namespace path::to::v2;
+	using namespace path::to::v2;
 }
 #endif
 
 using namespace path::to::v1;
 
-// TODO: test that forward declarations fail to compile when OLD_CODE_ENABLED is OFF
-// struct InitName;
+// TODO: test that forward declarations fail to compile when OLD_CODE_ENABLED is
+// OFF struct InitName;
 
-void g(SomeEnum val = A) { (void)val; }
+void g(SomeEnum val = A) {
+	(void)val;
+}
 
 TEST(Renames, NamespaceRename) {
-    Bar obj;
-    obj = Bar{};
+	Bar obj;
+	obj = Bar{};
 
-    Bar obj2(obj);
-    Bar obj3 = std::move(obj2);
+	Bar obj2(obj);
+	Bar obj3 = std::move(obj2);
 
-    static_assert(sizeof(obj) == 1);
-    static_assert(sizeof(obj2) == 1);
+	static_assert(sizeof(obj) == 1);
+	static_assert(sizeof(obj2) == 1);
 
-    f(obj);
-    f();
+	f(obj3);
+	f();
 
-    g();
-    g(B);
+	g();
+	g(B);
 }
