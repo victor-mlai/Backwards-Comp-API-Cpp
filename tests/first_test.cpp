bc5493e42cd21df0e206233f0ddd261c6eb7b1da
Munteanu Victor <munteanu.victor0201@gmail.com>
2023-02-11 14:29:10 +0200

    test both old and new code

--- tests/first_test.cpp
+++ tests/first_test.cpp
@@ -1,10 +1,29 @@
-
-
 #include "gtest/gtest.h"
 
-TEST(AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(6* 6, 42);
+template <typename T, typename V = void> constexpr bool is_incomplete = true;
+template <typename T> constexpr bool is_incomplete<T, std::enable_if_t<sizeof(T)>> = false;
+
+struct InitName;
+
+TEST(Renames, ForwardDecl) {
+    void fun(const InitName&);
+    static_assert(is_incomplete<InitName>);
+}
+
+#ifdef OLD_CODE_ENABLED
+// Todo: rename InitName to FinalName
+struct InitName {};
+#else
+// simply rename
+struct FinalName {};
+
+// then add this type alias
+using InitName = FinalName;
+#endif
+
+TEST(Renames, BasicAssertions) {
+    InitName obj;
+    obj = InitName{};
+
+    static_assert(sizeof(obj) == 1);
 }
