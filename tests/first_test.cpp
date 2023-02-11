4bf75ab35775731bbcc30601c9654e735f67b55e
Munteanu Victor <munteanu.victor0201@gmail.com>
2023-02-11 15:20:21 +0200

    test both old and new code

--- tests/first_test.cpp
+++ tests/first_test.cpp
@@ -1,15 +1,5 @@
 #include "gtest/gtest.h"
 
-template <typename T, typename V = void> constexpr bool is_incomplete = true;
-template <typename T> constexpr bool is_incomplete<T, std::enable_if_t<sizeof(T)>> = false;
-
-struct InitName;
-
-TEST(Renames, ForwardDecl) {
-    void fun(const InitName&);
-    static_assert(is_incomplete<InitName>);
-}
-
 #ifdef OLD_CODE_ENABLED
 // Todo: rename InitName to FinalName
 struct InitName {};
