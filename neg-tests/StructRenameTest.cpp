56d71ee35647fc9d90a00bfce348a218c558c17e
Munteanu Victor <munteanu.victor0201@gmail.com>
2023-05-01 12:28:41 +0300

    separate user and lib code

--- neg-tests/StructRenameTest.cpp
+++ neg-tests/StructRenameTest.cpp
@@ -1,7 +1,9 @@
 #include "StructRename.hpp"
 
+// test that forward declarations will fail to compile after the API change
+struct InitName;
+
 int main()
 {
-    // test that forward declarations will fail to compile after the API change
-    struct InitName;
+
 }
