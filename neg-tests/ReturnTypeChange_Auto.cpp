1612deac0a311a0a25b9fb55185beec20aafd71a
Munteanu Victor <munteanu.victor0201@gmail.com>
2023-10-10 01:30:58 +0300

    fix negative tests that they compile before the api change

--- neg-tests/ReturnTypeChange_Auto.cpp
+++ neg-tests/ReturnTypeChange_Auto.cpp
@@ -4,7 +4,9 @@ TEST(ReturnTypeChange_Auto, AutoFailures) {
     //const Strukt s_mut;
     //const auto& val = s_mut.GetMemF();
 
-    foo  // placeholder compile error.
+#ifndef BC_API_CHANGED
+    foo  // test the negative tests with a compile error.
+#endif
     //// should have said that it cannot compare floats and GetterRetT.... but it doesn't
     //ASSERT_FLOAT_EQ(val, 3.f) << val;
 }
