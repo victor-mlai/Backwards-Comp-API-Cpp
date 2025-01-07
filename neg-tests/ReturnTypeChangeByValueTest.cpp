a28682f3ce28a39ae50c94794c5833c39d62b27b
Munteanu Victor <munteanu.victor0201@gmail.com>
2025-01-07 13:15:48 +0200

    fix CI

--- neg-tests/ReturnTypeChangeByValueTest.cpp
+++ neg-tests/ReturnTypeChangeByValueTest.cpp
@@ -1,17 +1,17 @@
 #include "ReturnTypeChangeByValue.hpp"
 #include "gtest/gtest.h"
 
-struct OtherStrukt
-{
-	explicit OtherStrukt(float) {}
-};
+#include <type_traits> // std::is_same_v
+
+template <typename T>
+void Foo(T) requires std::is_same_v<T, float> {}
 
 TEST(ReturnTypeChangeByValue_Neg, Auto) {
     Strukt obj;
 	
-	// After the API Change, x is of type detail::GetterRetT
+	// After the API Change, x is of type `detail::GetterRetT`
 	auto x = obj.GetMemF();
 
-	// Constructor is explicit => cannot do the implicit conversion
-	OtherStrukt otherObj(x);
+	// Error: no overload found that accepts `detail::GetterRetT`
+	Foo(x);
 }
