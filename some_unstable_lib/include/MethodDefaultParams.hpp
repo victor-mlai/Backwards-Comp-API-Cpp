c326c6316394e63e789ed9bb08a3134b33625173
Munteanu Victor <munteanu.victor0201@gmail.com>
2025-02-15 23:45:36 +0200

    add one more default parameter

--- some_unstable_lib/include/MethodDefaultParams.hpp
+++ some_unstable_lib/include/MethodDefaultParams.hpp
@@ -3,25 +3,31 @@
 #ifndef BC_API_CHANGED
     // Change: make this method receive a struct with the default parameters
     void SomeMethod(
-        const int /*mandatory*/,
-        const bool /*opt1*/ = false,
-        const float /*opt2*/ = 1e-6
+        int /*mandatory*/,
+        bool /*opt1*/ = false,
+        float /*opt2*/ = 1e-6f,
+        int /*opt3*/ = 42
     ) {}
 #else
-    // Solution: If you just add the new SomeMethod, calling SomeMethod with
-    // just the mandatory parameters becomes ambiguous. The compiler won't
+    // Solution: If you would just overload SomeMethod, calling SomeMethod with
+    // just the mandatory parameters would be ambiguous. The compiler won't
     // know which of the 2 methods to choose. To tell it to prefer the newer
     // one we need to make the old one less specialized by making it a template
     template<int = 0>
     void SomeMethod(
-        const int /*mandatory*/,
-        const bool /*opt1*/ = false,
-        const float /*opt2*/ = 1e-6f
+        int /*mandatory*/,
+        bool /*opt1*/ = false,
+        float /*opt2*/ = 1e-6f,
+        int /*opt3*/ = 42
     ) {}
 
-    struct SomeMethodOpts { bool opt1 = false; float opt2 = 1e-6f; };
+    struct SomeMethodOpts {
+        bool opt1 = false;
+        float opt2 = 1e-6f;
+        int opt3 = 42;
+    };
     void SomeMethod(
-        const int /*mandatory*/,
+        int /*mandatory*/,
         SomeMethodOpts /*opts*/ = {}
     ) {}
 #endif
--- tests/MethodDefaultParamsTest.cpp
+++ tests/MethodDefaultParamsTest.cpp
@@ -2,14 +2,18 @@
 #include "gtest/gtest.h"
 
 TEST(StructRename, BasicAssertions) {
-    SomeMethod(7);
+    SomeMethod(7);  // This calls SomeMethod(int, SomeMethodOpts{});
     SomeMethod(7, true);
     SomeMethod(7, false, 1e-4f);
+    SomeMethod(7, false, 1e-4f, 21);
 
     // Test that the method also works with the new struct
 #ifdef BC_API_CHANGED
     SomeMethod(7, SomeMethodOpts{true});
     SomeMethod(7, SomeMethodOpts{.opt2 = 1e-4f});
+    SomeMethod(7, SomeMethodOpts{.opt3 = 21});
     SomeMethod(7, SomeMethodOpts{.opt1 = true, .opt2 = 1e-4f});
+    SomeMethod(7, SomeMethodOpts{.opt2 = 1e-4f, .opt3 = 21});
+    SomeMethod(7, SomeMethodOpts{.opt1 = true, .opt2 = 1e-4f, .opt3 = 21});
 #endif
 }
